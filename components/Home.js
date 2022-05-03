0a637e9b54d3b8f897d03a0e45c85b2f39a6ab2f
mukund jajadiya <mukundjajadiya2017@gmail.com>
2022-05-03 19:08:20 +0530

    reduce home page blog content

--- components/Home.js
+++ components/Home.js
@@ -8,7 +8,7 @@ const Home = ({data}) => {
     <a href="#">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{blog.attributes.title}</h5>
     </a>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{blog.attributes.description}</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{blog.attributes.description.substring(0,250)}</p>
    <Link href={`${blog.attributes.slug}`}>
    <a  className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Read more
